3e3990903432450e6bb4d0c57a37a952a0db64c1
Gabriel Saruhashi <gabesaruhashi@fb.com>
2017-07-10 13:35:54 -0700

    Setup recycler view and adapter

--- app/src/main/java/shag/com/shag/Activities/FeedActivity.java
+++ app/src/main/java/shag/com/shag/Activities/FeedActivity.java
@@ -1,15 +1,67 @@
 package shag.com.shag.Activities;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
+import java.util.ArrayList;
+
+import butterknife.ButterKnife;
+import shag.com.shag.Adapters.FeedAdapter;
+import shag.com.shag.Models.Event;
+import shag.com.shag.Other.DividerItemDecorator;
 import shag.com.shag.R;
 
 public class FeedActivity extends AppCompatActivity {
 
+    // list of tweets
+    ArrayList<Event> events;
+    // recycler view
+    RecyclerView rvEvents;
+    // the adapter wired to the new view
+    FeedAdapter adapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        ButterKnife.bind(this);
         setContentView(R.layout.activity_feed);
+
+        // initialize the list of tweets
+        events = new ArrayList<>();
+        // construct the adater from the data source
+        adapter = new FeedAdapter(events);
+        // initialize recycler view
+        rvEvents = (RecyclerView) findViewById(R.id.rvEvents);
+
+        // attach the adapter to the RecyclerView
+        rvEvents.setAdapter(adapter);
+        // Set layout manager to position the items
+        rvEvents.setLayoutManager(new LinearLayoutManager(this));
+
+        // add line divider decorator
+        RecyclerView.ItemDecoration itemDecoration = new
+                DividerItemDecorator(this, DividerItemDecorator.VERTICAL_LIST);
+        rvEvents.addItemDecoration(itemDecoration);
+
+        populateFeed();
+    }
+
+    public void populateFeed() {
+        Event fakeEvent = new Event();
+        fakeEvent.eventName = "Party at Zuck's";
+        fakeEvent.location = "Facebook Seattle";
+        fakeEvent.genre = "Partay";
+        fakeEvent.time = "4pm";
+        ArrayList<String> friends = new ArrayList<String>(3);
+        friends.add("Gabriel");
+        friends.add("Samra");
+        friends.add("Hana");
+        fakeEvent.friendsAtEvent = friends;
+
+        events.add(0, fakeEvent);
+        adapter.notifyItemInserted(events.size() - 1);
+        rvEvents.smoothScrollToPosition(0);
     }
 }
