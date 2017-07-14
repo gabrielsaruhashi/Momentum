df76613b8ddb49fdae2a87fd2f8039109b033125
Hana Pearlman <hanapearlman@fb.com>
2017-07-14 16:52:04 -0700

    Include fragment containers in create event flow

--- app/src/main/java/shag/com/shag/Activities/CreateEventActivity.java
+++ app/src/main/java/shag/com/shag/Activities/CreateEventActivity.java
@@ -3,21 +3,15 @@ package shag.com.shag.Activities;
 import android.os.Bundle;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 import android.widget.ExpandableListAdapter;
 import android.widget.ExpandableListView;
 import android.widget.ImageView;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import com.bumptech.glide.Glide;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
 import butterknife.ButterKnife;
-import shag.com.shag.Adapters.CategoriesListAdapter;
 import shag.com.shag.Fragments.DialogFragments.PickCategoryDialogFragment;
 import shag.com.shag.R;
 
@@ -34,34 +28,6 @@ public class CreateEventActivity extends AppCompatActivity {
         ButterKnife.bind(this);
         setContentView(R.layout.activity_create_event);
         // populates the category hash map
-        initData();
-
-        // get views
-        listView = (ExpandableListView) findViewById(R.id.lvCategories);
-        ivBanner = (ImageView) findViewById(R.id.ivBanner);
-
-        // sets up the adapter
-        listAdapter = new CategoriesListAdapter(this,listDataHeader, listHash);
-        listView.setAdapter(listAdapter);
-
-        // populate banner view
-        Glide.with(CreateEventActivity.this)
-                .load("http://via.placeholder.com/300.png")
-                .into(ivBanner);
-
-
-        // create listener for item child click
-        listView.setOnChildClickListener(new ExpandableListView.OnChildClickListener() {
-
-            @Override
-            public boolean onChildClick(ExpandableListView parent, View v, int groupPosition, int childPosition, long id) {
-                /* You must make use of the View v, find the view by id and extract the text as below*/
-                TextView tvCategoryItem= (TextView) v.findViewById(R.id.lblListItem);
-                String data= tvCategoryItem.getText().toString();
-                Toast.makeText(CreateEventActivity.this, "Picked event: " + data, Toast.LENGTH_LONG).show();
-                return true;  // i missed this
-            }
-        });
 
         // Begin the transaction
         FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
