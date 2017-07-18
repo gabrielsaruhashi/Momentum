1a9d888c902779759f941d6ad4d4885da974df09
Gabriel Saruhashi <gabesaruhashi@fb.com>
2017-07-18 10:04:43 -0700

    changed populate feed method

--- app/src/main/java/shag/com/shag/Fragments/FeedFragment.java
+++ app/src/main/java/shag/com/shag/Fragments/FeedFragment.java
@@ -52,8 +52,8 @@ public class FeedFragment extends Fragment implements PickCategoryDialogFragment
     // the adapter wired to the new view
     FeedAdapter adapter;
     FacebookClient client;
-
     FloatingActionButton myFab;
+    private static ArrayList<Long> facebookFriendsIds;
 
     // inflation happens inside onCreateView
     @Nullable
@@ -79,11 +79,11 @@ public class FeedFragment extends Fragment implements PickCategoryDialogFragment
                 DividerItemDecorator(rvEvents.getContext(), DividerItemDecorator.VERTICAL_LIST);
         rvEvents.addItemDecoration(itemDecoration);
 
-        //TODO: find the best place to ask for this permission
-        // oginManager.getInstance().logInWithReadPermissions(getActivity(), Arrays.asList("user_friends"));
+        // initialize facebook friendsIds
+        facebookFriendsIds = new ArrayList<Long>();
 
-        populateFeed();
-
+        // gets friends and call populatefeed() the first time
+        getFacebookFriends();
 
         // setups FAB to work
         myFab = (FloatingActionButton) v.findViewById(R.id.myFAB);
@@ -98,78 +98,58 @@ public class FeedFragment extends Fragment implements PickCategoryDialogFragment
         return v;
     }
 
-    public void populateFeed() {
+    public void getFacebookFriends() {
         client = ParseApplication.getFacebookRestClient();
         client.getFriendsUsingApp(
                 new GraphRequest.Callback() {
                     public void onCompleted(GraphResponse response) {
+                        // gets friends ids
                         try {
                             JSONArray friends = response.getJSONObject().getJSONArray("data");
                             for (int i = 0; i < friends.length(); i++) {
                                 User friend = User.fromJson(friends.getJSONObject(i));
-                                getFriendsEvents(friend.fbUserID);
+                                facebookFriendsIds.add(friend.fbUserID);
                             }
+                            // populates initial feed
+                            populateFeed();
                         } catch (JSONException e) {
                             e.printStackTrace();
                         }
                     }
                 }
         );
+    }
+    //TODO improve populate feed method
+    public void populateFeed() {
+        for (int i = 0; i < facebookFriendsIds.size(); i++) {
+            ParseQuery<ParseObject> query = new ParseQuery<ParseObject>("Event");
+            query.whereEqualTo("event_owner_fb_id", facebookFriendsIds.get(i));
+            query.findInBackground(new FindCallback<ParseObject>() {
+                @Override
+                public void done(List<ParseObject> itemList, ParseException e) {
+                    if (e == null) {
+                        for (ParseObject item : itemList) {
+                            //Convert each item found to an event
+                            Event event = Event.fromParseObject(item);
+                            //add event to list to be displayed
+                            events.add(event);
+                        }
 
-        /*
-        Event fakeEvent = new Event();
-        //fakeEvent.eventId = new Long(123);
-        fakeEvent.eventName = "Party at Zuck's";
-        fakeEvent.location = "Facebook Seattle";
-        fakeEvent.genre = "Partay";
-        fakeEvent.deadline = new Date();
-        fakeEvent.deadline.setTime(new Date().getTime() + 45*60000); //45 min from now
-        fakeEvent.time = "4pm";
-        fakeEvent.latLng = new LatLng(47.628883, -122.342606);
-        fakeEvent.eventOwner=fakeGabriel;
-        fakeEvent.participantsIds= new ArrayList<Long>();
-
-        ArrayList<String> friends = new ArrayList<String>(3);
-        friends.add("Gabriel");
-        friends.add("Samra");
-        friends.add("Hana");
-        fakeEvent.friendsAtEvent = friends;
-
-        events.add(0, fakeEvent);
-        adapter.notifyItemInserted(events.size() - 1);
-        rvEvents.smoothScrollToPosition(0);
-        /*
-
-        Event fakeEvent2 = new Event();
-        fakeEvent2.eventId = new Long(198);
-        fakeEvent2.eventName = "Party at Bill's";
-        fakeEvent2.location = "Facebook Seattle";
-        fakeEvent2.genre = "Professional Gathering";
-        fakeEvent2.time = "4pm";
-        fakeEvent2.latLng = new LatLng(47.621397, -122.338092);
-        fakeEvent2.eventOwner=fakeGabriel;
-        fakeEvent2.participantsIds= new ArrayList<Long>();
-
-        ArrayList<String> friends2 = new ArrayList<String>(3);
-        friends2.add("Gabriel");
-        friends2.add("Samra");
-        friends2.add("Hana");
-        fakeEvent2.friendsAtEvent = friends2;
-
-        events.add(0, fakeEvent2);
-        adapter.notifyItemInserted(events.size() - 1);
-        rvEvents.smoothScrollToPosition(0);
-
-        MapFragment fragment = new MapFragment();
-        Bundle bundle = new Bundle();
-        bundle.putParcelable("first event",fakeEvent);
-        bundle.putParcelable("second event", fakeEvent2);
-        fragment.setArguments(bundle);
-        getActivity().getSupportFragmentManager().beginTransaction().commit();*/
-        //TODO: replace with real populate: *.
-       
-
-
+                        //TODO: move this somewhere else, it is currently over-sorting
+                        //Sort the events shown to user in order of soonest deadline
+                        Collections.sort(events, new Comparator<Event>() {
+                            @Override
+                            public int compare(Event event, Event t1) {
+                                return event.deadline.compareTo(t1.deadline);
+                            }
+                        });
+                        adapter.notifyDataSetChanged();
+                    } else {
+                        Log.d("feedfragment", "Error: " + e.getMessage());
+                    }
+                }
+            });
+        }
     }
 
     // create category dialog fragment
@@ -200,34 +180,4 @@ public class FeedFragment extends Fragment implements PickCategoryDialogFragment
         rvEvents.smoothScrollToPosition(0);*/
     }
 
-    //query to find events belonging to an owner with a specified Facebook ID
-    public void getFriendsEvents(final long friendFbId) {
-        ParseQuery<ParseObject> query = new ParseQuery<ParseObject>("Event");
-        query.whereEqualTo("event_owner_fb_id", friendFbId);
-        query.findInBackground(new FindCallback<ParseObject>() {
-            @Override
-            public void done(List<ParseObject> itemList, ParseException e) {
-                if (e == null) {
-                    for (ParseObject item : itemList) {
-                        //Convert each item found to an event
-                        Event event = Event.fromParseObject(item);
-                        //add event to list to be displayed
-                        events.add(event);
-                    }
-
-                    //TODO: move this somewhere else, it is currently over-sorting
-                    //Sort the events shown to user in order of soonest deadline
-                    Collections.sort(events, new Comparator<Event>() {
-                        @Override
-                        public int compare(Event event, Event t1) {
-                            return event.deadline.compareTo(t1.deadline);
-                        }
-                    });
-                    adapter.notifyDataSetChanged();
-                } else {
-                    Log.d("feedfragment", "Error: " + e.getMessage());
-                }
-            }
-        });
-    }
 }
