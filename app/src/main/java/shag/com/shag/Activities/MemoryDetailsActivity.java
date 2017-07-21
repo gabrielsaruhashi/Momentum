dc75e9a01652fd3be17279a28a8bafbdb4cda282
Gabriel Saruhashi <gabesaruhashi@fb.com>
2017-07-20 23:21:59 -0700

    Fixed picture size

--- app/src/main/java/shag/com/shag/Activities/MemoryDetailsActivity.java
+++ app/src/main/java/shag/com/shag/Activities/MemoryDetailsActivity.java
@@ -13,8 +13,6 @@ import android.view.Display;
 import android.view.View;
 import android.widget.Button;
 import android.widget.GridView;
-import android.widget.ImageView;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.parse.GetCallback;
@@ -35,9 +33,9 @@ import shag.com.shag.Models.Memory;
 import shag.com.shag.R;
 
 public class MemoryDetailsActivity extends AppCompatActivity implements ImageAdapter.ImageZoomAdapterCallback{
-    @BindView(R.id.tvMemoryName) TextView tvMemoryName;
+    //@BindView(R.id.tvMemoryName) TextView tvMemoryName;
     @BindView(R.id.btAddPicture) Button btAddPicture;
-    @BindView(R.id.ivMemoryBannerPicture) ImageView ivMemoryBannerPicture;
+    // @BindView(R.id.ivMemoryBannerPicture) ImageView ivMemoryBannerPicture;
     GridView gridView;
 
     ImageAdapter imageAdapter;
@@ -83,7 +81,6 @@ public class MemoryDetailsActivity extends AppCompatActivity implements ImageAda
                 Intent photoPickerIntent = new Intent();
                 photoPickerIntent.setType("image/*");
                 photoPickerIntent.setAction(Intent.ACTION_GET_CONTENT);//
-                // allow multiple pic selection
                 photoPickerIntent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
                 startActivityForResult(Intent.createChooser(photoPickerIntent, "Select Picture"), SELECT_IMAGE);
             }
@@ -95,11 +92,12 @@ public class MemoryDetailsActivity extends AppCompatActivity implements ImageAda
     {
         super.onActivityResult(requestCode, resultCode, data);
 
-        // When an Image is picked
+        // when an Image is picked
         if (requestCode == SELECT_IMAGE && resultCode == RESULT_OK  && data != null) {
+
             // retrieve a collection of selected images
-            //ArrayList<Parcelable> list = data.getParcelableArrayListExtra(Intent.EXTRA_STREAM);
             ClipData clipData = data.getClipData();
+
             // iterate over these images
             if (clipData != null ) {
                 for (int i = 0; i < clipData.getItemCount(); i++) {
@@ -107,7 +105,7 @@ public class MemoryDetailsActivity extends AppCompatActivity implements ImageAda
                         Bitmap selectedImageBitmap = MediaStore.Images.Media.getBitmap(MemoryDetailsActivity.this.getContentResolver(), clipData.getItemAt(i).getUri());
                         // Convert it to byte
                         ByteArrayOutputStream stream = new ByteArrayOutputStream();
-                        selectedImageBitmap.compress(Bitmap.CompressFormat.JPEG, 70, stream);
+                        selectedImageBitmap.compress(Bitmap.CompressFormat.JPEG, 80, stream);
                         byte[] image = stream.toByteArray();
 
                         // save uploaded picture to the cloud as a parsefile
@@ -138,44 +136,13 @@ public class MemoryDetailsActivity extends AppCompatActivity implements ImageAda
                     }
                 }
         }
-        /*
-        if (requestCode == SELECT_IMAGE && resultCode == Activity.RESULT_OK) {
-            if (data != null) {
-              // add picture to the array
-                try {
-                  Bitmap selectedImageBitmap = MediaStore.Images.Media.getBitmap(MemoryDetailsActivity.this.getContentResolver(), data.getData());
-                  // Convert it to byte
-                  ByteArrayOutputStream stream = new ByteArrayOutputStream();
-                  selectedImageBitmap.compress(Bitmap.CompressFormat.JPEG, 70, stream);
-                  byte[] image = stream.toByteArray();
 
-                  // save uploaded picture to the cloud as a parsefile
-                  ParseFile file = new ParseFile("image.JPEG", image);
-                  file.saveInBackground();
-
-                  // current pictures
-                  ArrayList<ParseFile> currentPictures = memory.getPicturesParseFiles();
-                  // currentPictures.add(file);
-
-                  ParseQuery<ParseObject> query = ParseQuery.getQuery("Memory");
-                  //TODO query for memory, update row
-                  // query.getInBackground(memory.)
-                  // memory.setPicturesParseFiles(currentPictures);
-
-                  // TODO Update local array
-                  pictures.add(file);
-                  imageAdapter.notifyDataSetChanged();
-
-              } catch (IOException e) {
-                    e.printStackTrace();
-                }
-
-            } */
         } else if (resultCode == Activity.RESULT_CANCELED) { // in case user cancels selection
             Toast.makeText(MemoryDetailsActivity.this, "Cancelled", Toast.LENGTH_SHORT).show();
         }
     }
 
+    // initiate image zoom dialog
     private void showImageZoomDialog(Bitmap bm, int screenWidth, int screenHeight) {
         FragmentManager fm = getSupportFragmentManager();
         ImageZoomDialogFragment imageZoomDialogFragment = ImageZoomDialogFragment.newInstance(bm, screenWidth, screenHeight);
