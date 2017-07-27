fcf0e9e802a2e45b15d9e0147ce26b9be778a7df
Samrawit Belachew <samrabelachew@fb.com>
2017-07-27 11:58:33 -0700

    remove gabe's detail memory error

--- app/src/main/java/shag/com/shag/Activities/LoginActivity.java
+++ app/src/main/java/shag/com/shag/Activities/LoginActivity.java
@@ -46,6 +46,7 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_login);
         context = this;
         intent = getIntent();
