bc013816e8b26ba57116906cfb0775b23851ebbb
Gabriel Saruhashi <gabesaruhashi@fb.com>
2017-08-05 17:33:45 -0700

    Added result code check for invite friends

--- app/src/main/java/shag/com/shag/Activities/ChatActivity.java
+++ app/src/main/java/shag/com/shag/Activities/ChatActivity.java
@@ -1257,6 +1257,12 @@ public class ChatActivity extends AppCompatActivity implements CreatePollDialogF
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        // added check for Invite Friends Request Code
+        if (requestCode == INVITE_FRIENDS_ACTIVITY) {
+            // Do nothing
+        }
+
         int btn;
         Poll poll = polls.get(viewPosition);
         final List<String> choices = poll.getChoices();
